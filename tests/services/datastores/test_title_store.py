38060ce3deb4badb776130342a6a71e8d3f61c32
Yanxing Wang <yanxing@uber.com>
2016-10-10 16:18:33 -0700

    More tests

---
+++ tests/services/datastores/test_title_store.py
@@ -0,0 +1,44 @@
+import uuid
+
+import mock
+from bootcamp.models.title import Title
+from bootcamp.services.datastores.title_store import TitleStore
+from tornado.testing import gen_test, AsyncTestCase
+
+
+class TestTitleStore(AsyncTestCase):
+    @mock.patch.object(Title, 'query')
+    @gen_test
+    def test_get_titles(self, mock_query):
+        mock_query.return_value = [1, 2, 3]
+
+        titles = yield TitleStore().get_titles()
+
+        mock_query.assert_called_once_with()
+        self.assertEquals(titles, [1, 2, 3])
+
+    @gen_test
+    def test_get_title(self):
+        fake_uuid = uuid.uuid4()
+        title = yield TitleStore().get_title(fake_uuid)
+        self.assertIsNotNone(title)
+
+    @mock.patch.object(Title, 'persist')
+    @mock.patch('bootcamp.lib.database.get_db_session')
+    @gen_test
+    def test_create_from_entity(self, mock_get_db, mock_persist):
+        title_entity = Title(
+            title_id='ABC-123',
+            title='test title 1',
+            video_path='test',
+            file_names='test file',
+            description='test des',
+            video_size=1000000000,
+            rate=8,
+        )
+        mock_persist.return_value = None
+        mock_get_db.return_value = mock.Mock()
+
+        yield TitleStore().create_from_entity(title_entity)
+
+        mock_persist.assert_called_once_with()
---
+++ tests/services/test_title_service.py
@@ -0,0 +1,62 @@
+import uuid
+import mock
+from bootcamp.services.datastores.title_store import TitleStore
+from bootcamp.services.title_service import TitleService
+from tornado import gen
+from tornado.testing import gen_test, AsyncTestCase
+
+from bootcamp.models.title import Title
+
+
+class TestTitleService(AsyncTestCase):
+    @mock.patch.object(TitleStore, 'create_from_entity')
+    @mock.patch.object(TitleService, 'handle_title_added')
+    @gen_test
+    def test_create_title_with_entity(self, mock_handle_added, mock_create):
+        fake_title = mock.Mock(
+            uuid=uuid.uuid4(),
+            title_id='ABC-123',
+            title='test title 1',
+        )
+        mock_create.return_value = gen.maybe_future(fake_title)
+        mock_handle_added.return_value = gen.maybe_future(None)
+
+        title_entity = Title(
+            title_id='ABC-123',
+            title='test title 1',
+            video_path='test',
+            file_names='test file',
+            description='test des',
+            video_size=1000000000,
+            rate=8,
+        )
+        title = yield TitleService().create_title_with_entity(title_entity)
+
+        mock_create.assert_called_once_with(title_entity)
+        mock_handle_added.assert_called_once_with(fake_title)
+
+        self.assertEquals(title.title, title_entity.title)
+        self.assertEquals(title.title_id, title_entity.title_id)
+
+    @mock.patch.object(TitleStore, 'get_title')
+    @gen_test
+    def test_get_title(self, mock_get):
+        fake_title = mock.Mock()
+        mock_get.return_value = gen.maybe_future(fake_title)
+
+        fake_uuid = uuid.uuid4()
+        title = yield TitleService().get_title(fake_uuid)
+
+        mock_get.assert_called_once_with(fake_uuid)
+        self.assertEquals(title, fake_title)
+
+    @mock.patch.object(TitleStore, 'get_titles')
+    @gen_test
+    def test_get_titles(self, mock_get):
+        fake_title = mock.Mock()
+        mock_get.return_value = gen.maybe_future(fake_title)
+
+        title = yield TitleService().get_titles()
+
+        mock_get.assert_called_once_with()
+        self.assertEquals(title, fake_title)
